f88b6821c7f9fd4e587e9e199bba0761c319f1b6
Brandon Harrison <brandonharrisoncode@gmail.com>
2020-05-30 11:12:00 -0600

    Pull joke from api

--- src/components/Joke.js
+++ src/components/Joke.js
@@ -1,16 +1,70 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 
-const Joke = props => (
-  <header id="header" style={props.timeout ? { display: 'none' } : {}}>
-    <div className="content">
-      <div className="inner">
-        <h1>Dad-A-Base Entry #316</h1>
-        <p>A fully responsive site template designed by</p>
-      </div>
-    </div>
-  </header>
-)
+class Joke extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.controller = new AbortController()
+    this.signal = this.controller.signal
+
+    this.fetchJoke = this.fetchJoke.bind(this)
+    this.componentWillUnmount = this.componentWillUnmount.bind(this)
+
+    this.state = {
+      joke: '',
+    }
+  }
+
+  async componentDidMount() {
+    this.setState({
+      joke: '',
+    })
+
+    this.fetchJoke()
+  }
+
+  async fetchJoke() {
+    const signal = this.signal
+
+    const url = 'https://the-dad-a-base-api.herokuapp.com/random'
+    fetch(url, { signal })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Error while accessing joke.')
+        }
+        return response.json()
+      })
+      .then(parsedJSON => {
+        this.setState({
+          joke: parsedJSON.joke,
+        })
+      })
+      .catch(error => {
+        if (error.name === 'AbortError') {
+          return
+        }
+
+        console.error(error)
+        console.error('Unable to access the api at:', url)
+      })
+  }
+
+  render() {
+    const joke = this.state.joke
+
+    return (
+      <header id="header" style={this.props.timeout ? { display: 'none' } : {}}>
+        <div className="content">
+          <div className="inner">
+            <h1>Dad-A-Base Entry #316</h1>
+            <p>{joke}</p>
+          </div>
+        </div>
+      </header>
+    )
+  }
+}
 
 Joke.propTypes = {
   joke: PropTypes.string,
